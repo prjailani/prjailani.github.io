03419572ff780f8f3afeea9668488492c5c609c9
Abdul Kader Jailani P R <jailanijailani968@gmail.com>
2023-02-21 22:02:08 +0530

    Check with Google API

--- index.js
+++ index.js
@@ -1,18 +1,31 @@
-// Get the user's current position
 navigator.geolocation.getCurrentPosition(function(position) {
     var lat = position.coords.latitude;
     var lng = position.coords.longitude;
   
-    // Display the initial location
     console.log("Initial location: " + lat + ", " + lng);
   
-    // Update the location in real-time
     navigator.geolocation.watchPosition(function(position) {
       var lat = position.coords.latitude;
       var lng = position.coords.longitude;
   
-      // Display the updated location
       console.log("Updated location: " + lat + ", " + lng);
     });
   });
-  
+
+const API_KEY = "AIzaSyBdmF37Lbgn623YZYckDGPZMqYAHnhDFMQ";
+var STOP_NAME = prompt("Enter your stop name: ");
+const URL = `https://maps.googleapis.com/maps/api/geocode/json?address=${STOP_NAME}&key=${API_KEY}`;
+
+fetch(URL)
+  .then(response => response.json())
+  .then(data => {
+    const location = data.results[0].geometry.location;
+    const latitude = location.lat;
+    const longitude = location.lng;
+    
+    console.log(`Latitude: ${latitude}`);
+    console.log(`Longitude: ${longitude}`);
+  })
+  .catch(error => {
+    console.error(error);
+  });
