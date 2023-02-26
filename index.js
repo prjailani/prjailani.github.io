bfca5be8f490c24bf9937dbbba7fbe3ebab51803
Abdul Kader Jailani P R <jailanijailani968@gmail.com>
2023-02-26 12:35:42 +0530

    testing latlong because it doesn't allow on local

--- index.js
+++ index.js
@@ -12,111 +12,48 @@ navigator.geolocation.getCurrentPosition(function(position) {
     });
   });
 
-// const API_KEY = "AIzaSyBdmF37Lbgn623YZYckDGPZMqYAHnhDFMQ";
+function initAutocomplete() {
+  const addressInput = document.getElementById('address-input');
 
-// const stopName = prompt("Enter a stop name:");
-
-// const URL = `https://maps.googleapis.com/maps/api/geocode/json?address=${stopName}&key=${API_KEY}`;
-
-// fetch(URL)
-//   .then(response => response.json())
-//   .then(data => {
-//     const matches = data.results;
-    
-//     console.log(`Available options for "${stopName}":`);
-//     matches.forEach(match => {
-//       console.log(`- ${match.formatted_address}`);
-//     });
-//   })
-//   .catch(error => {
-//     console.error(error);
-//   });
-
- function initAutocomplete() {
-      const addressInput = document.getElementById('address-input');
-      const searchBox = new google.maps.places.SearchBox(addressInput);
-
-      searchBox.addListener('places_changed', () => {
-        const places = searchBox.getPlaces();
-        if (places.length === 0) {
-          return;
-        }
-        const address = places[0].formatted_address;
-        const lat = places[0].geometry.location.lat();
-        const lng = places[0].geometry.location.lng();
-        console.log(`Address: ${address}, Latitude: ${lat}, Longitude: ${lng}`);
-      });
-
-      addressInput.addEventListener('input', () => {
-        const input = addressInput.value;
-        if (input.length > 2) {
-          const url = `https://nominatim.openstreetmap.org/search?q=${input}&format=json&addressdetails=1&limit=5`;
-          fetch(url)
-            .then(response => response.json())
-            .then(data => {
-              const suggestions = data.map(item => item.display_name);
-              showSuggestions(suggestions);
-            })
-            .catch(error => console.log(error));
-        }
-      });
-
-      function showSuggestions(suggestions) {
-        const suggestionList = document.getElementById('suggestion-list');
-        suggestionList.innerHTML = '';
-        suggestions.forEach(suggestion => {
-          const li = document.createElement('li');
-          li.textContent = suggestion;
-          li.addEventListener('click', () => {
-            addressInput.value = suggestion;
-            suggestionList.innerHTML = '';
-          });
-          suggestionList.appendChild(li);
-        });
-      }
-
-      document.addEventListener('click', event => {
-        const suggestionList = document.getElementById('suggestion-list');
-        if (!suggestionList.contains(event.target)) {
-          suggestionList.innerHTML = '';
-        }
-      });
+  addressInput.addEventListener('input', () => {
+    const input = addressInput.value;
+    if (input.length > 2) {
+      const url = `https://nominatim.openstreetmap.org/search?q=${input}&format=json&addressdetails=1&limit=5`;
+      fetch(url)
+        .then(response => response.json())
+        .then(data => {
+          const suggestions = data.map(item => item.display_name);
+          showSuggestions(suggestions);
+        })
+        .catch(error => console.log(error));
     }
-    function initAutocomplete() {
-      const addressInput = document.getElementById('address-input');
+  });
 
-      addressInput.addEventListener('input', () => {
-        const input = addressInput.value;
-        if (input.length > 2) {
-          const url = `https://nominatim.openstreetmap.org/search?q=${input}&format=json&addressdetails=1&limit=5`;
-          fetch(url)
-            .then(response => response.json())
-            .then(data => {
-              const suggestions = data.map(item => item.display_name);
-              showSuggestions(suggestions);
-            })
-            .catch(error => console.log(error));
-        }
-      });
-
-      function showSuggestions(suggestions) {
-        const suggestionList = document.getElementById('suggestion-list');
+  function showSuggestions(suggestions) {
+    const suggestionList = document.getElementById('suggestion-list');
+    suggestionList.innerHTML = '';
+    suggestions.forEach(suggestion => {
+      const li = document.createElement('li');
+      li.textContent = suggestion;
+      li.addEventListener('click', () => {
+        addressInput.value = suggestion;
         suggestionList.innerHTML = '';
-        suggestions.forEach(suggestion => {
-          const li = document.createElement('li');
-          li.textContent = suggestion;
-          li.addEventListener('click', () => {
-            addressInput.value = suggestion;
-            suggestionList.innerHTML = '';
-          });
-          suggestionList.appendChild(li);
-        });
-      }
-
-      document.addEventListener('click', event => {
-        const suggestionList = document.getElementById('suggestion-list');
-        if (!suggestionList.contains(event.target)) {
-          suggestionList.innerHTML = '';
-        }
       });
+      suggestionList.appendChild(li);
+    });
+  }
+
+  document.addEventListener('click', event => {
+    const suggestionList = document.getElementById('suggestion-list');
+    if (!suggestionList.contains(event.target)) {
+      suggestionList.innerHTML = '';
     }
+  });
+}
+function latlon() {
+  const location = document.querySelector("#address-input");
+  const jsonvalues = `https://nominatim.openstreetmap.org/search/${location}?format=json&addressdetails=1&limit=1&polygon_svg=1`;
+  console.log($.getJSON(jsonvalues[0]));
+}
+var submitButton = document.getElementById("submit");
+submitButton.addEventListener("click",latlon);
